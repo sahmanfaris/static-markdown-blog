31d6b6f7bbf5bf5bd3f888d2342bc8fafcaa7a8e
@farissahman1 <farissahman1@gmail.com>
2021-10-28 22:07:20 +0200

    implemented search component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,25 +1,27 @@
-import Head from 'next/head'
-import Header from './Header'
+import Head from "next/head";
+import Header from "./Header";
+import Search from "./Search";
 
 const Layout = ({ title, keywords, description, children }) => {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta name='keywords' content={keywords} />
-        <meta name='description' content={description} />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className='container mx-auto my-7'>{children}</main>
+      <Search />
+      <main className="container mx-auto my-7">{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
 Layout.defaultProps = {
-  title: 'Welcome to DevSpace',
-  keywords: 'development, coding, programming',
-  description: 'The best info and news in development',
-}
+  title: "Welcome to DevSpace",
+  keywords: "development, coding, programming",
+  description: "The best info and news in development",
+};
